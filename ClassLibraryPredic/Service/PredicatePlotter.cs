fe5c7fc7d31038678fdd97718f9841b441932cdf
matveybardack <matbardack@gmail.com>
2025-11-06 17:35:37 +0300

    commit 6: Дописал в сервисы метод для построения графика. Надо получить с него PlotModel.

---
+++ ClassLibraryPredic/Service/PredicatePlotter.cs
@@ -0,0 +1,61 @@
+﻿using ClassLibraryPredic;
+using ClassLibraryPredic.Interface;
+using ClassLibraryPredic.Models;
+using OxyPlot;
+using OxyPlot.Series;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class PredicatePlotter
+{
+    /// <summary>
+    /// Создаёт PlotModel для области истинности предиката (максимум 2 переменные).
+    /// Каждая точка, где предикат истиннен, отображается на графике.
+    /// </summary>
+    public static PlotModel CreatePlotModel(IPredicate predicate, Domain domain, string title = "Область истинности предиката")
+    {
+        var vars = domain.Variables.ToArray();
+        if (vars.Length != 2)
+            throw new System.ArgumentException("Метод поддерживает ровно 2 переменные для графика.");
+
+        string xVar = vars[0];
+        string yVar = vars[1];
+
+        // Вычисляем все комбинации, где предикат истиннен
+        var truthAssignments = PredicateAnalyzer.ComputeTruthSet(predicate, domain);
+
+        // Создаём PlotModel
+        var plotModel = new PlotModel { Title = title };
+
+        var scatterSeries = new ScatterSeries
+        {
+            MarkerType = MarkerType.Circle,
+            MarkerSize = 4,
+            MarkerFill = OxyColors.SkyBlue
+        };
+
+        // Добавляем точки
+        foreach (var assignment in truthAssignments)
+        {
+            double x = Convert.ToDouble(assignment[xVar]);
+            double y = Convert.ToDouble(assignment[yVar]);
+            scatterSeries.Points.Add(new ScatterPoint(x, y));
+        }
+
+        plotModel.Series.Add(scatterSeries);
+
+        // Настройка осей
+        plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis
+        {
+            Position = OxyPlot.Axes.AxisPosition.Bottom,
+            Title = xVar
+        });
+        plotModel.Axes.Add(new OxyPlot.Axes.LinearAxis
+        {
+            Position = OxyPlot.Axes.AxisPosition.Left,
+            Title = yVar
+        });
+
+        return plotModel;
+    }
+}
--- WpfAppPredic/MainWindowGraph.xaml.cs
+++ WpfAppPredic/MainWindowGraph.xaml.cs
@@ -1,6 +1,10 @@
-﻿using OxyPlot;
+﻿using ClassLibraryPredic;
+using OxyPlot;
+using ClassLibraryPredic.Models;
 using OxyPlot.Annotations;
+using OxyPlot.Axes;
 using OxyPlot.Series;
+using OxyPlot.Wpf;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,52 +30,22 @@ namespace WpfAppPredic
         {
             InitializeComponent();
 
-            //Тестовый график
-            this.MyModel = new PlotModel { Title = "Example 1" };
+            // Тест графика
+            var factory = PredicateParser.Parse("∀ x : x < 2*y");
 
-            // --- Граница области ---
-            var line = new FunctionSeries(x => 2 * x, -5, 5, 0.1, "y = 2x");
-            MyModel.Series.Add(line);
+            // Создаём область значений
+            var domain = new Domain();
+            domain.SetDomain("x", Enumerable.Range(0, 10).Cast<object>());
+            domain.SetDomain("y", Enumerable.Range(0, 20).Cast<object>());
 
-            var region = new PolygonAnnotation
-            {
-                Fill = OxyColor.FromAColor(120, OxyColors.SkyBlue), // прозрачный цвет
-                StrokeThickness = 0
-            };
+            // Получаем IPredicate
+            var predicate = factory(domain);
 
-            // Определяем многоугольник под прямой
-            double xMin = -5;
-            double xMax = 5;
-            double yMin = -10; // нижняя граница области
-            double yOnMinX = 2 * xMin;
-            double yOnMaxX = 2 * xMax;
+            // Создаём PlotModel
+            var model = PredicatePlotter.CreatePlotModel(predicate, domain, "y < 2*x && x > 0");
 
-            region.Points.Add(new DataPoint(xMin, yMin));   // нижний левый
-            region.Points.Add(new DataPoint(xMax, yMin));   // нижний правый
-            region.Points.Add(new DataPoint(xMax, yOnMaxX)); // точка на прямой
-            region.Points.Add(new DataPoint(xMin, yOnMinX)); // точка на прямой
-
-            MyModel.Annotations.Add(region);
-
-            // --- Настройка осей ---
-            MyModel.Axes.Add(new OxyPlot.Axes.LinearAxis
-            {
-                Position = OxyPlot.Axes.AxisPosition.Bottom,
-                Title = "X",
-                Minimum = -5,
-                Maximum = 5
-            });
-
-            MyModel.Axes.Add(new OxyPlot.Axes.LinearAxis
-            {
-                Position = OxyPlot.Axes.AxisPosition.Left,
-                Title = "Y",
-                Minimum = -10,
-                Maximum = 10
-            });
-
-            //this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
-            TestModel.Model = MyModel;
+            // Передаём model в OxyPlot WPF PlotView
+            TestModel.Model = model;
         }
     }
 }
